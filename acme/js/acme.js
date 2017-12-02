1920c1db45c5af47cd32ff47fa8ee16f4b535b8a
Colby <ironflame24@yahoo.com>
2017-12-02 12:24:49 -0700

    update

--- acme/js/acme.js
+++ acme/js/acme.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $.getJSON("js/acme.json", function(json){
+        
         $("#listI1").append(json.Anvils[0].name);
         $("#listI2").append(json.Explosives[0].name);
         $("#listI3").append(json.Decoys[0].name);
